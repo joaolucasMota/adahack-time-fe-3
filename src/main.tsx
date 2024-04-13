292ab86649255abe2bd46282a40eee5694f0c5e6
Joseph Matheus <josephmatheus@hotmail.com>
2024-04-13 11:34:48 -0300

    :sparkles: feat: Componente Header

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import ReactDOM from 'react-dom/client'
-import './styles/index.css'
 import React from 'react'
+import { GlobalStyles } from './styles/global'
+import { Header } from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyles />
+    <Header />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle`
+export const GlobalStyles = createGlobalStyle`
         * {
     margin: 0;
     padding: 0;
