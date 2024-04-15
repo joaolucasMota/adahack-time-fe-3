4baa56cf15ea4fe67ed40ad1488362354d080feb
BR-Darkness <BRvitorgalindo@gmail.com>
2024-04-15 04:29:05 -0300

    fix: homepage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,41 +2,45 @@ import { useEffect, useState } from "react";
 import * as S from "./styled"
 import axios from "axios";
 
-import { ButtonNewFeedback } from "../../components/ButtonNewFeedback";
-import { EmpresaHeader } from "../../components/EmpresaHeader";
-import { CardEmpresa } from "../../components/CardEmpresa"
-import { TagPCDs } from "../../components/TagPCDs"
+import { NewFeedbackButton } from "../../components/NewFeedbackButton";
+import { SectionHeader } from "../../components/SectionHeader";
+import { TagComponent } from "../../components/TagComponent";
+import { CompanyCard } from "../../components/CompanyCard"
 import { BASE_URL } from "../../util/api";
 
 export const HomePage = () => {
 
-  const [EmpresasData, setEmpresasData] = useState<EmpresaProps[]>([]); 
+    const [CompanyData, setCompanyData] = useState<EmpresaProps[]>([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get(BASE_URL);
-        setEmpresasData(response.data);
-      } catch (error) {
-        console.log("Erro na requisição:", error);
-      }
-    } fetchData();
-  }, [EmpresasData]);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(BASE_URL);
+                setCompanyData(response.data);
+            } catch (error) {
+                console.log("Erro na requisição:", error);
+            }
+        } fetchData();
+    }, [CompanyData]);
 
-  return (
-    <>
-      <EmpresaHeader nome="Corp Solutions Corp Solutions Corp ApenasUmTesteDeTextoMuitoLongoParaAQuebraDeLinhaNoMobile" setor="Recursos Humanos" />
-      <S.Container>
-        <p>Hello World</p>
-      </S.Container>
-      <br />
-      {EmpresasData.map(empresa => <CardEmpresa key={empresa.nome} empresa={empresa} />)}
-      <br />
-      <TagPCDs possuiVagas={true} />
-      <br />
-      <ButtonNewFeedback />
-      <br />
-    </>
-  )
+    return (
+        <>
+            <SectionHeader 
+                variant="homepage"
+                mainText="Corp Solutions Corp Solutions Corp ApenasUmTesteDeTextoMuitoLongoParaAQuebraDeLinhaNoMobile"
+                subText="Recursos Humanos" 
+            />
+            <br />
+            <S.Container>
+                <p>Hello World</p>
+            </S.Container>
+            <br />
+            {CompanyData.map(empresa => <CompanyCard key={empresa.nome} empresa={empresa} />)}
+            <br />
+            <TagComponent variant="PCDs" textSize="medium" text="Está empresa possui vagas para PCDs" />
+            <br />
+            <NewFeedbackButton />
+            <br />
+        </>
+    )
 }
-
