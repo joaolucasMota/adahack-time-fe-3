d96a98f734baffb94e00674fb5cbaff33ad19785
BR-Darkness <BRvitorgalindo@gmail.com>
2024-04-16 08:11:48 -0300

    refactor: merge footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,28 +1,23 @@
+import { TagComponent } from "../TagComponent";
 import * as S from "./styled";
-
-import { ALargeSmall, Contrast, PersonStanding } from "lucide-react"
-
 
 export const Footer = ( ) =>{
     return(
-            <S.FooterContainer>
-                <S.CompanyName><p>Corp Solutions™ - © 2024 </p></S.CompanyName>
-                <S.Nav>
-                        <div>
-                            <PersonStanding />
-                            <span>Opções de Acessibilidade</span>
-                        </div>
-                    <S.Ul>
-                        <li onClick={() => { /*futura função de acessibilidade*/ }}>
-                            <ALargeSmall />
-                            <span>Alternar tamanho da fonte</span>
-                        </li>
-                        <li onClick={() => { /*futura função de acessibilidade*/ }}>
-                            <Contrast />
-                            <span>Alternar contraste</span>
-                        </li>
-                    </S.Ul>
-                </S.Nav>
-            </S.FooterContainer>
-        )
+        <S.FooterContainer>
+            <S.CompanyName><p>Corp Solutions™ - © 2024 </p></S.CompanyName>
+            <S.Nav>
+                <S.Bold><TagComponent variant="acessibilidade" text="Opções de Acessibilidade: " /></S.Bold>
+                
+                <S.FlexDiv>
+                    <button onClick={() => { /*futura função de acessibilidade*/ }}>
+                        <TagComponent variant="tamanho-fonte" text="Alternar tamanho da fonte" />
+                    </button>
+
+                    <button onClick={() => { /*futura função de acessibilidade*/ }}>
+                        <TagComponent variant="contraste" text="Alternar contraste" />
+                    </button>
+                </S.FlexDiv>
+            </S.Nav>
+        </S.FooterContainer>
+    )
 }
