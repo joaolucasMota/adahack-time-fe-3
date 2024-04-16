40883e5a1970651477ea3736fe4d392d569009c7
BR-Darkness <BRvitorgalindo@gmail.com>
2024-04-16 08:25:13 -0300

    feat: update ErrorPage

--- src/pages/ErrorPage/Index.tsx
+++ src/pages/ErrorPage/Index.tsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom"
+import { Button } from "../../components/Button"
 import * as S from "./styled"
 
 export const ErrorPage = () => {
     return (
         <S.FlexContainer>
             <h1><span>Erro 404:</span> Página não Encontrada</h1>
-            <Link to="/">Voltar para o Início</Link>
+            <Button to="/" text="Voltar para o Início" />
         </S.FlexContainer>
     )
 }
