b20bb88285843665e67c2a42e22645865ef83968
BR-Darkness <BRvitorgalindo@gmail.com>
2024-04-16 22:07:28 -0300

    feat: update Company Page

--- src/pages/CompanyPage/Index.tsx
+++ src/pages/CompanyPage/Index.tsx
@@ -1,20 +1,57 @@
+import { EmpresaProps } from "../../@types/types"
+import { useEffect, useState } from "react"
+// import { BASE_URL } from "../../util/api"
 import * as S from "./styled"
+import axios from "axios"
 
-import { Header } from "../../components/Header"
 import { SectionHeader } from "../../components/SectionHeader"
+import { Header } from "../../components/Header"
 import { Footer } from "../../components/Footer"
+import { useParams } from "react-router-dom"
+import { BASE_URL } from "../../util/api"
+import { TagFeedback } from "../../components/TagFeedback"
+import { TagComponent } from "../../components/TagComponent"
 
 export const CompanyPage = () => {
+
+    const [CompanyData, setCompanyData] = useState<EmpresaProps>();
+
+    const {id} = useParams();
+
+    useEffect(() => {
+        async function fetchData(id:string|undefined) {
+            try {
+                const response = await axios.get(`${BASE_URL}/${id}`);
+                setCompanyData(response.data);
+            } catch (error) {
+                console.log("Erro na requisição:", error);
+            }
+        } fetchData(id);
+    }, [id]);
+
     return (
         <S.PageContainer>
             <Header />
             <SectionHeader 
                 page="company"
-                mainText="...Aqui vai o nome da empresa"
-                subText="...Aqui vai o setor da empresa"
+                mainText={CompanyData?.nome}
+                subText={CompanyData?.setor}
             />
 
             {/* Conteudo */}
+
+            <S.CompanyInfoContainer>
+                <h1>Sobre a Empresa:</h1>
+                <pre>{CompanyData?.descricao}</pre>
+                <S.CompanyInfoTagsContainer>
+                    <TagFeedback variant={CompanyData?.mediaAvaliacao}/>
+                    <TagComponent variant="endereco" text={CompanyData?.local} />
+                    <TagComponent variant="vagas" text={`${String(CompanyData?.qtdVagas)} vaga(s)`} />
+                    {
+                        CompanyData?.pcd && <TagComponent variant="PCDs" text="Está empresa possui vagas para PCDs" />
+                    }
+                </S.CompanyInfoTagsContainer>
+            </S.CompanyInfoContainer>
 
             <Footer />
         </S.PageContainer>
--- src/pages/CompanyPage/styled.ts
+++ src/pages/CompanyPage/styled.ts
@@ -3,3 +3,29 @@ import styled from "styled-components";
 export const PageContainer = styled.div`
 
 `;
+
+export const CompanyInfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+
+    h1
+    {
+        font-size: 24px;
+    }
+
+    pre
+    {
+        max-width: 720px;
+        white-space: pre-wrap;
+        font-family: inherit;
+        text-align: justify;
+        line-height: 1.5;
+    }
+`;
+
+export const CompanyInfoTagsContainer = styled.div`
+    flex-wrap: wrap;
+    display: flex;
+    gap: .5em 2em;
+`;
